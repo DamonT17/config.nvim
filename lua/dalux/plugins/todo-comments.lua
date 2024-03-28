52467e238e4b429bb1b694171d974120d1d079a8
DamonT17 <damontregear@gmail.com>
2024-03-28 10:13:01 -0700

    plug(todo): modified todo-comments plugin

--- lua/dalux/plugins/todo-comments.lua
+++ lua/dalux/plugins/todo-comments.lua
@@ -3,7 +3,27 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    signs = true,
-  },
+  config = function()
+    local todo = require('todo-comments')
+    todo.setup({
+      signs = true,
+    })
+
+    -- [[ Todo Comments keymaps ]]
+    local wk = require('which-key')
+    wk.register({
+      ['[t'] = {
+        function()
+          todo.jump_prev()
+        end,
+        'Previous [T]odo Comment',
+      },
+      [']t'] = {
+        function()
+          todo.jump_next()
+        end,
+        'Next [T]odo Comment',
+      },
+    }, { mode = 'n' })
+  end,
 }
