dc117aab84a3cd3c2e23a1d194645788baf29df4
DamonT17 <damontregear@gmail.com>
2025-03-26 20:40:31 -0700

    plugins(debug): added debug support for C/C++

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -1,69 +1,83 @@
---[[ 
 return {
+  -- [[ Plugin: mfussenegger/nvim-dap ]]
+  -- NOTE: See `:help dap.txt` or https://github.com/mfussenegger/nvim-dap for more info
+  -- Each language requires installation and configuration of its own debug adapter
   'mfussenegger/nvim-dap',
   dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Installs the debug adapters for you
+    {
+      'rcarriga/nvim-dap-ui',
+      dependencies = {
+        'nvim-neotest/nvim-nio',
+      },
+    },
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
   },
   config = function()
     local dap = require('dap')
     local dapui = require('dapui')
 
     require('mason-nvim-dap').setup({
-      automatic_setup = true,
-
-      handlers = {},
-
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'codelldb',
       },
+      automatic_installation = true,
+      handlers = {},
     })
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
-    end, { desc = 'Debug: Set Breakpoint' })
-
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup({
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    })
-
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result' })
-
+    -- [[ Plugin: nvim-dap-ui ]]
+    -- NOTE: See `:help nvim-dap-ui.txt` or https://github.com/rcarriga/nvim-dap-ui for more info
+    dapui.setup({})
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup()
+    -- Configure adapters
+    dap.adapters.codelldb = {
+      tye = 'executable',
+      command = 'codelldb',
+    }
+
+    -- C/C++ configuration
+    dap.configurations.cpp = {
+      {
+        name = 'Launch',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+        env = function()
+          local variables = {}
+          for k, v in pairs(vim.fn.environ()) do
+            table.insert(variables, string.format('%s=%s', k, v))
+          end
+          return variables
+        end,
+      },
+    }
+    dap.configurations.c = dap.configurations.cpp
+
+    -- [[ Keymaps ]]
+    ---Maps a key sequence to a function with a description.
+    ---@param keys string Sequence of keys
+    ---@param func any Function to call
+    ---@param desc string Description of the keymap
+    local map = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { desc = desc })
+    end
+
+    map('<F5>', dap.continue, 'Debug: Start/Continue')
+    map('<F1>', dap.step_into, 'Debug: Step Into')
+    map('<F2>', dap.step_over, 'Debug: Step Over')
+    map('<F3>', dap.step_out, 'Debug: Step Out')
+    map('<F7>', dapui.toggle, 'Debug: See last session result')
+    map('<leader>b', dap.toggle_breakpoint, 'Debug: Toggle [B]reakpoint')
+    map('<leader>B', function()
+      dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+    end, 'Debug: Set [B]reakpoint')
   end,
-} ]]
+}
