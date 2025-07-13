ef65e1af4253f488c85422782085b0a52f0c150a
DamonT17 <damontregear@gmail.com>
2025-07-12 20:44:45 -0700

    plugins(lazy): update lazy.nvim bootstrap & config

--- init.lua
+++ init.lua
@@ -15,9 +15,18 @@
 -- [[ Bootstrap `lazy.nvim` plugin manager ]]
 -- NOTE: See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -35,10 +44,10 @@ require('custom.commands')
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup('custom.plugins', {
-  change_detection = {
-    notify = false,
+  checker = {
+    enabled = true,
+    notify = true,
   },
-  checker = { enable = true },
   ui = {
     border = 'rounded',
     title = ' Lazy Plugin Manager ',
